6a9c9785db67a887f99db1b1ead2cb29f3f2dc18
Nur Maulidiyah(NYTLYS) <nur.maulidiyah20@gmail.com>
2016-12-20 01:16:37 +0700

    update body and meta

--- application/views/p/html/head.php
+++ application/views/p/html/head.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
     
