41a41fa22b9d1d476026935f5bca5b5f42343588
Nur Maulidiyah(NYTLYS) <nur.maulidiyah20@gmail.com>
2017-01-04 19:28:42 +0700

    register

--- application/views/register.php
+++ application/views/register.php
@@ -1,29 +1,27 @@
-<div class="login-box">
-        <div class="container">
-              <div class="sbm"><a href="<?= base_url(''); ?>login" class="btn btn-success btn-quirk" style="font-size:26px; padding:6px 20px;">Masuk</a></div>
-              <div class="sbm"><a href="<?= base_url(''); ?>register" class="btn btn-success btn-quirk" style="font-size:26px; padding:6px 20px;">Daftar</a></div>
+<div class="row login">
+  <div class="container">
+    <form class="row" method="post">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <h3 class="col-xs-12">Pendaftaran</h3>
         </div>
-
-        <div class="container">
-        <div id="infoMessage"><?php echo $message;?></div>
-        <div id="infoMessage"><h2>Pendaftaran</h2></div>
-        <?php echo form_open("register");?>
-              <div class="input">
-                <?php //echo lang('login_identity_label', 'identity');?>
-                <span><i class="fa fa-user"></i></span>
-                <?php echo form_input($usernamereg, '', "required='required' placeholder='Username'");?>
-              </div>
-                <div class="input">
-                <?php //echo lang('login_identity_label', 'identity');?>
-                <span><i class="fa fa-user"></i></span>
-                <?php echo form_input($emailreg, '', "required='required' placeholder='Email'");?>
-              </div>
-              <div class="input">
-                <?php //echo lang('login_password_label', 'password');?>
-                <span><i class="fa fa-lock"></i></span>
-                <?php echo form_input($passwordreg, '', "required='required' placeholder='Password'");?>
-              </div>
-              <div class="sbm"><?php echo form_submit('submit', 'Daftar');?></div>
-            <?php echo form_close();?>
+      </div>
+      <?php if($message){?>
+        <div id="infoMessage" class="col-md-6 col-md-offset-3 alert alert-danger"><?=$message;?></div>
+      <?php } ?>
+      <div class="form-box col-md-6 col-md-offset-3">
+        <div class="form-group">
+          <?php echo form_input($usernamereg, '', "required='required' placeholder='Username' class='form-control'");?>
         </div>
-</div><!-- end login-box -->
+        <div class="form-group">
+          <?php echo form_input($emailreg, '', "required='required' placeholder='Email' class='form-control'");?>
+        </div>
+        <div class="form-group">          <?php echo form_input($passwordreg, '', "required='required' placeholder='Password' class='form-control'");?>
+        </div>
+        <div class="form-group">
+          <?php echo form_submit('submit', 'Daftar', 'class="btn btn-success"');?>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
